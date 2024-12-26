67e3c62adf84d76a619adc03bb9267f863d7d659
rajkumarkashyap3582 <rajkumarkashyap3582@gmail.com>
2024-12-26 10:46:25 +0530

    sdfghj

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ app.use(cors());
 app.use('/api', routes);
 
 // Start the server
-app.listen(5000, () => {
-  console.log('Server running on http://localhost:5000');
+app.listen(3005, () => {
+  console.log('Server running on http://localhost:3005');
 });
