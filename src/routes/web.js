d636371d87d3ee08d23234a2ac7087fbef273990
Sachin <prajapatsachin835@gmail.com>
2025-01-04 15:31:17 +0530

    sac

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const authController = require('../controllers/authController');
 const googleController = require('../controllers/googleController');
+
+const { validateRegistration, handleValidationErrors } = require('../middleware/validateRegistration');
 const apiBindController = require('../controllers/apiBindController');
 const showCoinController = require('../controllers/showCoinController');
 const passport = require('passport');
@@ -12,6 +14,14 @@ const router = express.Router();
 const app = express();
 app.use(express.json()); // Handles `application/json` content
 app.use(express.urlencoded({ extended: true }));
+router.get('/', (req , res)=>{
+res.send('hello');
+});
+
+router.post('/forget', authController.forgetValidator, authController.formForget)
+router.post('/reset', authController.resetValidator, authController.resetPass)
+// router.post('/verify-otp', verifyOtp);
+router.post('/verify-otp',authController.verifyOtp);
 
 router.get('/google',googleController.googleLogin);
 router.post('/register', validateRegistration,handleValidationErrors,authController.formRegister);
@@ -21,31 +31,6 @@ router.get('/coins',showCoinController.showCoin);
 router.post('/apiBind',apiBindController.apiBind);
 router.get('/account-info', spotTradeController.getAccountInfo);
 router.get('/future-account-info', futureTradeController.getFutureAccountInfo);
-// Google Authentication Routes
-
-
-module.exports = router;
-
-const express = require('express');
-const authController = require('../controllers/authController');
-const googleController = require('../controllers/googleController');
-const passport = require('passport');
-const { validateRegistration, handleValidationErrors } = require('../middleware/validateRegistration');
-
-const router = express.Router();
-const app = express();
-app.use(express.json()); // Handles `application/json` content
-app.use(express.urlencoded({ extended: true }));
-router.get('/', (req , res)=>{
-res.send('hello');
-});
-router.get('/google',googleController.googleLogin);
-router.post('/register', validateRegistration,handleValidationErrors,authController.formRegister);
-router.post('/login',authController.loginValidator,authController.loginHandler);
-router.post('/forget', authController.forgetValidator, authController.formForget)
-router.post('/reset', authController.resetValidator, authController.resetPass)
-// router.post('/verify-otp', verifyOtp);
-router.post('/verify-otp',authController.verifyOtp);
 // Google Authentication Routes
 
 
